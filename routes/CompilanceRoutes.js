bae23795cb21c77c26a5529f3dd538c458f85196
Aryam2121 <aryamangupta2121@gmail.com>
2025-03-06 18:54:21 +0530

    update Routes

--- routes/CompilanceRoutes.js
+++ routes/CompilanceRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getReports,createReport, updateReport,deleteReport } from "../controllers/CompilanceController.js";
 const router = express.Router();
-router.get("/getReports",  getReports);
-router.post("/addReports",  createReport);
-router.put("/updatereport/:id",  updateReport);
-router.delete("/deletereport/:id",  deleteReport);
+router.get("/getReports", getReports);
+router.post("/addReports", createReport);
+router.put("/updateReport/:id", updateReport);
+router.delete("/deleteReport/:id", deleteReport);
 
 export default router;
