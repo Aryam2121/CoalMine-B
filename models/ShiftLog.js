a5e4663ce6f60de665701f3d5a1477893105b58e
Aryam2121 <aryamangupta2121@gmail.com>
2025-03-29 09:55:23 +0530

    update shift log modal

--- models/ShiftLog.js
+++ models/ShiftLog.js
@@ -5,7 +5,7 @@ const ShiftLogSchema = new mongoose.Schema(
   {
     shiftDetails: {
       type: String,
-      required: [true, 'Shift details are required'], 
+      required: [false, 'Shift details are required'], 
       minlength: [10, 'Shift details must be at least 10 characters long'], 
     },
     // shiftDate: {
@@ -14,11 +14,11 @@ const ShiftLogSchema = new mongoose.Schema(
     // },
     shiftStartTime: {
       type: String, // Time format could be 'HH:mm'
-      required: [true, 'Shift start time is required'], 
+      required: [false, 'Shift start time is required'], 
     },
     shiftEndTime: {
       type: String, // Time format could be 'HH:mm'
-      required: [true, 'Shift end time is required'], 
+      required: [false, 'Shift end time is required'], 
     },
     status: {
       type: String,
@@ -31,7 +31,7 @@ const ShiftLogSchema = new mongoose.Schema(
     },
   },
   {
-    timestamps: true, // Automatically adds createdAt and updatedAt fields
+    timestamps: false, // Automatically adds createdAt and updatedAt fields
   }
 );
 
