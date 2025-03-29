364f07525f3144bc0a5fe62d81c8bdf4265bcf34
Aryam2121 <aryamangupta2121@gmail.com>
2025-03-29 10:39:45 +0530

    update dependency

--- routes/shiftLogs.js
+++ routes/shiftLogs.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // Routes for shift logs
 router.get('/getAllLogs', getAllShiftLogs);
 router.get('/getshift/:id', getShiftLogById);
-router.post('/createLogs', upload.single('file'), createShiftLog);
+router.post('/createLogs', upload.single('file'),createShiftLog);
 router.put('/updateLog/:id', updateShiftLog);
 router.delete('/deleteLog/:id', deleteShiftLog);
 
