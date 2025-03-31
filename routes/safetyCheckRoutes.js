c54abcaa6096c6dd05163392fd21811d3aa71484
Aryam2121 <aryamangupta2121@gmail.com>
2025-03-31 11:14:17 +0530

    update routes

--- routes/safetyCheckRoutes.js
+++ routes/safetyCheckRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { submitSafetyCheck, getUserSafetyChecks } from "../controllers/safetyCheckController.js";
-import { protect } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
-router.post("/safety-check", protect, submitSafetyCheck);
-router.get("/getAllsafety-check", protect, getUserSafetyChecks);
+router.post("/safety-check", submitSafetyCheck);
+router.get("/getAllsafety-check", getUserSafetyChecks);
 
 export default router;
